99da5b46c0963c520ec4c28fef47476db2c98382
SergePugachev <sergeypugachev96@gmail.com>
2024-05-14 11:53:02 +0300

    Commit

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from model_vit import result_vit
 from model_bert import result_bert
 from model_cat import result_cat
 
+'changes'
 
 vit = gr.Interface(fn=result_vit, inputs=gr.Image(type="numpy"),
                    outputs=[gr.Number(label="допустимое", precision=5), gr.Number(label="недопустимое", precision=5)])
